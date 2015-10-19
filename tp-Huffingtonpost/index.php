5cabdc53e45426c382d43d9f5290f25926071b16
Valentin Deville <contact@valentin-deville.eu>
2015-10-19 12:19:42 +0200

    Initial commit

---
+++ tp-Huffingtonpost/index.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>123</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+    <link rel="stylesheet" href="css/custom.css">
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body id="background">
+<div class="container">
+    <header>
+        <div class="container-fluid">
+            <div class="row" id="top">
+                <h4>12 octobre 2015</h4>
+                <h1>Le Huffingtonpost</h1>
+                <h4 id="h2-top">En association avec le groupe <img class="img-responsive" src="http://evasas.eelv.fr/files/2012/07/Le-Monde-Logo-1024x227.png">
+                </h4>
+            </div>
+            <div class="row" id="top2">
+                <div class="col-md-4"><p>Edition: <span style="font-weight: bold;">FR <i class="glyphicon glyphicon-menu-down"></i></span></p></div>
+                <div class="col-md-4">
+                    <p>
+                        <input id="recherche" type="text" class="form-control" placeholder="Chercher sur le HuffPost">
+                    </p>
+                </div>
+                <div class="col-md-4">
+                    <p>
+                        <a href="#">
+                            <img title="It's trap !" data-toggle="tooltip" data-placement="top" src="images/social.png" alt="Social" style="float: right">
+                        </a>
+                    </p>
+                </div>
+            </div>
+            <div class="navbar-sm">
+                <div class="navbar-primary">
+                    <nav class="navbar navbar-static-top" role="navigation">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav">
+                                <li class="active"><a href="#">A la lune</a></li>
+                                <li><a href="#">Politique</a></li>
+                                <li><a href="#">Economique</a></li>
+                                <li><a href="#">International</a></li>
+                                <li><a href="#">Culture</a></li>
+                                <li><a href="#">Médias</a></li>
+                                <li><a href="#">People</a></li>
+                                <li><a href="#">Le bon lien</a></li>
+                                <li><a href="#">C'est demain</a></li>
+                            </ul>
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a href="#">C'est la vie</a></li>
+                            </ul>
+                        </div><!-- /.navbar-collapse -->
+                    </nav>
+                </div>
+            </div>
+            <ul id="list-nav">
+                <li>Crise au pouvoir</li>
+                <li>Têtes chercheuses</li>
+                <li>La preuve en images</li>
+                <li>Environement</li>
+                <li>Alimentation</li>
+                <li>Réussir autrement</li>
+                <li>Déconnecter pour respirer</li>
+                <li>Vie de bureau</li>
+                <li>Ca marche</li>
+            </ul>
+            <hr>
+        </div>
+    </header>
+</div>
+<div class="container" id="content">
+    <div class="row">
+        <div class="col-md-12">
+            <h2 id="grosTitre">Seuil<span style="color: #853B3C;">s</span> de pauvreté</h2>
+            <h3 id="sousTitre">Le Prix Nobel d'économie récompense les travaux sur la consommation
+            et la pauvreté selon les lieux et les époques.</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <img src="http://fakeimg.pl/1920x1080/?text=You have problem ? Lel." class="img-responsive">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <h3 id="sousTitre">Angus Deaton, lauréat du Prix Nobel d'économie 2015</h3>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <p class="text-center" style="font-weight: bold;">
+                    <a href="#">Commentaires</a> | <a href="#">Prix Nobel</a>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="row" style="border-top: solid 1px rgba(0,0,0, 0.4);">
+        <div class="col-md-4" style="border-right: solid 2px rgba(0,0,0, 0.4); margin-top: 10px;">
+            <h4 id="titre1">Blog à la une</h4>
+            <div class="row">
+                <div class="col-md-12">
+                    <p id="author">
+                        <img style="float: left; margin-right: 10px;" src="http://fakeimg.pl/80x80/" class="img-responsive" id="pp">
+                        <b>Michel Eltchinanof</b><br />Rédacteur en chef expert en connerie.</span>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <a href="#"><p id="titre2">Hillary Clinton aura du mal à retourner à la Maison Blanche</p></a>
+                    <p>INTERNATIONAL - A l'heure actuelle, il est impossible de prédire le rapport de force de novembre 2016,
+                        tant l'environnement électoral est volatile et tant la course reste longue jusqu'à
+                        l'élection générale. Mais la campagne électorale est en train de tourner à la
+                        défaveur des démocrates, ce qui nourrit leur nervosité à l'égard de leur seule
+                        et unique candidate sérieuse. 2016 pourrait bien être l'année de l'alternance à la Maison-Blanche.
+                    </p>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-12">
+                    <a href="#"><p id="titre2">Au Canada, c'est déjà Halloween grâce aux élections fédérales</p></a>
+                    <p>
+                        INTERNATIONAL - "C'est carrément Halloween avant le temps", me fait
+                        remarquer ma fille qui attend la fin du mois avec impatience.
+                        En effet, au Québec, plusieurs électeurs se sont présentés à leurs
+                        bureaux de vote, costumés, le visage intégralement couvert... d'un sac de
+                        patates, d'un masque de clown, d'un casque de ski ou d'une cagoule. Ce mouvement
+                        citoyen fait suite à une saga judiciaire intéressante...
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p id="author">
+                        <img style="float: left; margin-right: 10px;" src="http://fakeimg.pl/80x80/" class="img-responsive" id="pp">
+                        <b>Alberto Rototo</b><br />Organisateur de teufs de ouf</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4" style="padding-top: 25px; border-right: solid 2px rgba(0,0,0, 0.4); margin-top: 10px;">
+            <div class="row">
+                <div class="col-md-12">
+                    <a href="#"><p id="titre2">Sean O'Brien suspendu pour son coup de poing à Papé</p></a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="http://fakeimg.pl/439x320/282828/eae0d0/" class="img-responsive">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p id="commentA">
+                        <a href="#">Commentaires (17)</a> | <a href="#">International</a>
+                    </p>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-12">
+                    <a href="#"><p id="titre2">Le très vilain coup de poing irlandais à Papé qui aurait mérité un carton rouge</p></a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="http://fakeimg.pl/439x320/282828/eae0d0/" class="img-responsive">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p id="commentA">
+                        <a href="#">Commentaires (7)</a> | <a href="#">Rugby</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4" style="padding-top: 25px;">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="https://i.gyazo.com/d7e8e9ad5aaa791ebec13027bc4a7aca.png" class="img-responsive">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container" id="footer">
+    <div class="row">
+        <div class="col-md-12">
+            <h3>Footer</h3>
+            <h4>12 octobre 2015</h4>
+            <h1>Le Huffingtonpost</h1>
+            <h4 id="h2-top">En association avec le groupe <img class="img-responsive" src="http://evasas.eelv.fr/files/2012/07/Le-Monde-Logo-1024x227.png">
+            </h4>
+        </div>
+    </div>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script>
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+</script>
+</body>
+</html>
