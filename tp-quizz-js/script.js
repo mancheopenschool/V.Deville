9c6af187d4c643535e80405f4998cd79cdf2dbf1
Valentin Deville <contact@valentin-deville.eu>
2015-11-27 00:46:13 +0100

    Add enw features

---
+++ tp-quizz-js/script.js
@@ -0,0 +1,70 @@
+/**
+ * Created by valentin on 26/11/15.
+ */
+
+    // nQ = Numéro question
+    // Q = Question
+    // R = Réponse réelle
+    // r = Propositions
+
+$(document).ready(function(){
+
+    /*
+
+    $.getJSON('./question.json', function(json){
+     var nbrQuestions = Object.keys(json).length;
+     for (var i = 1; i <= nbrQuestions; i++){
+
+     console.log(json[i].nQ);
+
+     $('<p class="question">'+ json[i].Q +'</p>').appendTo($('div'));
+
+     console.log(json[i].R);
+
+     var nbrReponses = Object.keys(json[i]['r']).length;
+
+     for (var e = 0; e < nbrReponses; e++){
+     $('<input type="radio" name="' + json[i].nQ + '" checked value="' + json[i].r[e] + '"> ' + json[i].r[e] + '<br />').appendTo($('div'));
+     }
+     }
+     });
+
+     */
+
+});
+
+var mettage = $('.quizz');
+
+function nbrQuestions(){
+    var test;
+    $.getJSON('./question.json', function(json){
+        test= Object.keys(json).length;
+    });
+    return test;
+}
+
+function nombreAlea(min, max){
+    return Math.floor(Math.random() * max + min);
+}
+
+function getQuestion(num){
+    $.getJSON('./question.json', function(json){
+        $('<p class="question">'+ json[num].Q +'</p>').appendTo(mettage);
+    })
+}
+
+function getReponse(num){
+    $.getJSON('./question.json', function(json){
+        var nbrReponses = Object.keys(json[num]['r']).length;
+        for (var e = 0; e < nbrReponses; e++){
+            $('<input type="radio" name="' + json[num].nQ + '" value="' + json[num].nQ + json[num].r[e] + '"> ' + json[num].r[e] + '<br />').appendTo(mettage);
+                                                                    //  ^ Rajout d'un identifiant sous forme de chiffre arabe unitaire non décimale propre à la question pour éviter eventuelle bug avec d'autre questions
+        }
+    })
+}
+
+function getQR(num){
+    mettage.empty();
+    getQuestion(num);
+    getReponse(num);
+}
